67fe79a959d0effd6b8ff29273a9bf5c1085e9be
Y0h4n3s <yohaksa80@gmail.com>
2020-11-19 09:43:21 -0500

    .

---
+++ bash/ssti-flask-subclasses-parser.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+file=$1;
+blob=`cat $1`
+trimmedBlob=`echo "$blob" | sed -e 's/\<\\<class\>//g' | sed -e 's/\<enum\>//g' | tr -d '<>,'`
+classes=($trimmedBlob)
+
+index=0
+for i in ${classes[*]};
+do
+  echo $index":" $i
+  index=$(($index+1))
+done
